88cc5e4332a4402179795013c70f2bd331209b68
JPM1919 <153214837+JPM1919@users.noreply.github.com>
2025-02-13 10:22:06 +0100

    Creación del proyecto Spring

---
+++ helloworld-vscode/src/main/java/es/codeurjc/helloworld_vscode/HelloworldVscodeApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.helloworld_vscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldVscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldVscodeApplication.class, args);
+	}
+
+}
---
+++ helloworld-vscode/src/test/java/es/codeurjc/helloworld_vscode/HelloworldVscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.helloworld_vscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldVscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
