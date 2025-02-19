9de19e8706023ab0b8227410859b0010509f8d5e
ifernandezru22 <i.fernandezru.2022@gmail.com>
2025-02-19 14:16:03 +0100

    Modificación de atributos de la clase Exercise

--- gymapp/src/main/java/es/codeurjc/gymapp/model/Exercise.java
+++ gymapp/src/main/java/es/codeurjc/gymapp/model/Exercise.java
@@ -1,11 +1,40 @@
 package es.codeurjc.gymapp.model;
 
-import jakarta.persistence.*;
-
-@Entity
-public class Exercise {
+public class Exercise extends ModelEntity{
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private String description;
+    private Material material;
+    private Routine routine;
+
+    public Exercise() {
+    }
+
+    public Exercise(String description, Material material) {
+        this.description = description;
+        this.material = material;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public Routine getRoutine() {
+        return routine;
+    }
+
+    public void setRoutine(Routine routine){
+        this.routine = routine;
+    }
 }
