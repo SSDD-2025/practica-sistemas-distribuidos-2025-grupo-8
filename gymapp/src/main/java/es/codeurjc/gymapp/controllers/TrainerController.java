54f220796ebeffa6bf0d87ba12d434e38bde256c
JPM1919 <153214837+JPM1919@users.noreply.github.com>
2025-02-26 20:08:46 +0100

    ahora se pueden borrar entrenadores si eres admin, más solucion de bugs

--- gymapp/src/main/java/es/codeurjc/gymapp/controllers/TrainerController.java
+++ gymapp/src/main/java/es/codeurjc/gymapp/controllers/TrainerController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,29 +44,40 @@ public class TrainerController {
 		return "trainer";
 	}
 
-    @PostMapping("/trainer/{id}")
+    @GetMapping("/trainer/{id}")
     public String getTrainer(Model model, @PathVariable Long id) {
         Optional<Trainer> trainer = trainerServices.findById(id);
-        model.addAttribute("trainer", trainer.get());
+        model.addAttribute("trainer", trainer.get().getName());
         model.addAttribute("logged", userSession.isLoggedIn());
         return "trainerDetails"; 
     }
 
     @PostMapping("/trainer/{id}/addOrReplace")
-    public String addOrReplace(Model model, @PathVariable Long id) { //select trainer 
+    public String addOrReplace(Model model, @PathVariable Long id) { //select personal trainer 
         Optional<Trainer> trainer = trainerServices.findById(id);
         User user = userServices.findByName(userSession.getName()).get();
         user.setTrainer(trainer.get());
         userServices.save(user);
         trainer.get().addUser(user);
         trainerServices.save(trainer.get());    
-        return "trainer"; 
+        return "personalTrainer"; 
     }
 
 	@PostMapping("/trainer/add")
 	public String addTrainer(Model model) {		
 		return "trainerAdd"; 
 	}
+
+    @PostMapping("/trainer/{id}/delete")
+	public String deleteTrainer(Model model, @PathVariable Long id) {		
+		Optional<Trainer> trainer = trainerServices.findById(id);
+        for (User user : trainer.get().getUsers()) {
+            user.setTrainer(null);
+            userServices.save(user);
+        }
+        trainerServices.deleteById(id);    
+        return "trainerDeleted";
+    }
 
 	@PostMapping("/trainer/add/form")
 	public String addTrainerForm(Model model, @RequestParam String name) {		
