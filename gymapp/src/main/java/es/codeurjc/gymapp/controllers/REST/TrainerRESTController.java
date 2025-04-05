d6fd3f6a9a19f563ad66a3abdd4b9f7aaa1a9b08
4ndresito3 <andresito.korotkov@gmail.com>
2025-04-05 20:29:55 +0200

    Add TrainerRESTController class and finished it, change in TrainerServices

--- gymapp/src/main/java/es/codeurjc/gymapp/controllers/REST/TrainerRESTController.java
+++ gymapp/src/main/java/es/codeurjc/gymapp/controllers/REST/TrainerRESTController.java
@@ -1,10 +1,90 @@
 package es.codeurjc.gymapp.controllers.REST;
 
+import java.net.URI;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import es.codeurjc.gymapp.DTO.Trainer.TrainerDTO;
+import es.codeurjc.gymapp.DTO.Trainer.TrainerSimpleDTO;
+import es.codeurjc.gymapp.DTO.User.UserDTO;
+import es.codeurjc.gymapp.DTO.User.UserSimpleDTO;
+import es.codeurjc.gymapp.model.Trainer;
+import es.codeurjc.gymapp.model.UserSession;
+import es.codeurjc.gymapp.services.TrainerServices;
 
 @RestController
 @RequestMapping("/trainer")
 public class TrainerRESTController {
 
+    @Autowired
+    private TrainerServices trainerServices;
+
+    //GetMapping parts
+    
+    @GetMapping("/")
+    //TODO: Change name and implementation
+    //Posible implementation Log in
+    // For admin to see a dashboard maybe
+    public ResponseEntity<List<TrainerSimpleDTO>> getTrainers() {
+        if(trainerServices.findAllSimple().isEmpty())
+            return ResponseEntity.notFound().build();
+        List<TrainerSimpleDTO> trainers = trainerServices.findAllSimple();
+        return ResponseEntity.ok(trainers);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<TrainerDTO> getTrainer(@PathVariable Long id){
+        if(trainerServices.findDTOById(id).isEmpty())
+            return ResponseEntity.notFound().build();
+        TrainerDTO trainer = trainerServices.findDTOById(id).get();
+        return ResponseEntity.ok(trainer);
+    }
+
+    //PostMapping parts
+
+    @PostMapping("/")
+    public ResponseEntity<TrainerDTO> postTrainer(@RequestBody TrainerDTO trainerDTO) {
+        trainerServices.save(trainerDTO);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(trainerDTO.id()).toUri();
+
+        return ResponseEntity.created(location).body(trainerDTO);
+    }
+
+    //PutMapping parts
+
+    @PutMapping("/{id}")
+    public TrainerDTO putTrainer(@PathVariable Long id, @RequestBody TrainerDTO trainerDTO) {
+        if(trainerServices.findDTOById(id).isEmpty())
+            throw new NoSuchElementException();
+        TrainerDTO newTrainer = new TrainerDTO(id, trainerDTO.name(), trainerDTO.description(), 
+            trainerDTO.imageFile(), trainerDTO.users(), trainerDTO.comments());
+        return newTrainer;
+    }
+
+    //DeleteMapping parts
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TrainerDTO> deleteTrainer(@PathVariable Long id){
+        if(trainerServices.findDTOById(id).isEmpty())
+            return ResponseEntity.notFound().build();
+        
+        TrainerDTO trainerDTO = trainerServices.findDTOById(id).get();
+        trainerServices.deleteById(id);
+        return ResponseEntity.ok(trainerDTO);
+    }
 }
