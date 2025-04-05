2713ab7b654cb7f76173778dbd73e005ddcbb12b
4ndresito3 <andresito.korotkov@gmail.com>
2025-04-05 20:26:55 +0200

    Finish UserRESTController and remove some comments

--- gymapp/src/main/java/es/codeurjc/gymapp/controllers/REST/UserRESTController.java
+++ gymapp/src/main/java/es/codeurjc/gymapp/controllers/REST/UserRESTController.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,7 +50,6 @@ public class UserRESTController {
     //PostMapping parts
 
     @PostMapping("/")
-    //TODO: Sign up
     public ResponseEntity<UserDTO> postUser(@RequestBody UserDTO userDTO) {
         userServices.save(userDTO);
 
@@ -64,7 +62,6 @@ public class UserRESTController {
     //PutMapping parts
 
     @PutMapping("/{id}")
-    //TODO: Change fields
     public UserDTO putUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
         if(userServices.findById(id).isEmpty())
             throw new NoSuchElementException();
@@ -77,8 +74,12 @@ public class UserRESTController {
 
     //DeleteMapping parts
     @DeleteMapping("/{id}")
-    //TODO: Delete user from DB
-    public void deleteUser(@PathVariable Long id){
-
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable Long id){
+        if(userServices.findById(id).isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        UserDTO userDTO = userServices.findById(id).get();
+        userServices.deleteById(id);
+        return ResponseEntity.ok(userDTO);
     }
 }
