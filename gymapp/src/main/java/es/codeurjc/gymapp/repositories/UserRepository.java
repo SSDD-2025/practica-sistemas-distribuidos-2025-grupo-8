d0f5a3f9065ce5e9caeb8ef132dec1516148c5ef
Andres <andresito.korotkov@gmail.com>
2025-04-09 14:28:20 +0200

    Remove unused imports

--- gymapp/src/main/java/es/codeurjc/gymapp/repositories/UserRepository.java
+++ gymapp/src/main/java/es/codeurjc/gymapp/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package es.codeurjc.gymapp.repositories;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import es.codeurjc.gymapp.model.User;
 
