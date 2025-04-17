4dc24a41fd4b11436837317e82eb6ba6defb3005
4ndresito3 <andresito.korotkov@gmail.com>
2025-04-17 16:11:22 +0200

    Refactor UserSimpleDTO to replace isAdmin with roles parameter for better role management

--- gymapp/src/main/java/es/codeurjc/gymapp/DTO/User/UserSimpleDTO.java
+++ gymapp/src/main/java/es/codeurjc/gymapp/DTO/User/UserSimpleDTO.java
@@ -3,6 +3,6 @@ package es.codeurjc.gymapp.DTO.User;
 import java.sql.Blob;
 
 public record UserSimpleDTO(Long id, String name, 
-                            Blob imageFile, Boolean isAdmin) {
+                            Blob imageFile, String... roles) {
 
 }
