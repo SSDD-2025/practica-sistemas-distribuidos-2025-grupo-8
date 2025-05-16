9b2eba23a94f8052bbec72c3d3f5d96ecf683182
4ndresito3 <andresito.korotkov@gmail.com>
2025-05-16 17:58:37 +0200

    Finished docker compose

---
+++ docker/publish_compose.ps1
@@ -0,0 +1,7 @@
+if ($args.Count -ne 1) {
+    Write-Error "Uso: .\publish_compose.ps1 <usuario>"
+    exit 1
+}
+$usuario = $args[0]
+$env:DOCKER_USER = $usuario
+docker compose -f ./docker-compose.prod.yml publish $usuario/gymapp:latest --with-env
